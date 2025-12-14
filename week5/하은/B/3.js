20e043823d4a89570a551b3a51fb1cecf6f435a6
조하은 <jhe93@naver.com>
2025-12-14 10:08:23 +0900

    add: 정리본 추가 (5~8주차)

---
+++ week5/하은/B/3.js
@@ -0,0 +1,44 @@
+function randMax(max) {
+    return Math.trunc(1e9 * Math.random()) % max;
+}
+
+var reel = {
+    symbols: ['♠', '♥', '♦', '♣', '☺', '★', '(', '~'],
+    spin() {
+        if (this.position == null) {
+            this.position = randMax(this.symbols.length - 1);
+        }
+        this.position = (this.position + 100 + randMax(100)) % this.symbols.length;
+    },
+    display() {
+        if (this.position == null) {
+            this.position = randMax(this.symbols.length - 1);
+        }
+        return this.symbols[this.position];
+    },
+};
+
+var slotMachine = {
+    reels: [Object.create(reel), Object.create(reel), Object.create(reel)],
+    spin() {
+        this.reels.forEach(function spinReel(reel) {
+            reel.spin();
+        });
+    },
+    display() {
+        // ...여기에 코드를 작성하세요...
+        this.reels.forEach((reel) => reel.display());
+    },
+};
+
+slotMachine.spin();
+slotMachine.display();
+// ♠ | ★ | ♥
+// ♦ | ♠ | ☺
+// ☺ | ♥ | ★
+
+slotMachine.spin();
+slotMachine.display();
+// ♦ | ♠ | ♣
+// ♣ | ♥ | ☺
+// ☺ | ♦ | ★
